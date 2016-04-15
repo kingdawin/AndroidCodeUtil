e553710c75ea9d6d8ec40e553da8f797d1064cff
kingdawin <397546372@qq.com>
2016-04-15 15:38:36 +0800

    first commit

---
+++ AndroidCodeHelper/app/src/main/java/com/dawin/androidhelper/viewpager_fragment/BaseFragment.java
@@ -0,0 +1,38 @@
+package com.dawin.androidhelper.viewpager_fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * ViewPage+Fragment优化
+ *
+ * ViewPage会预加载第二个页面，取消预加载页面的方法:
+ * Fragment的setUserVisibleHint和getUerVisibleHint方法
+ */
+
+public abstract class BaseFragment extends Fragment {
+    //fragment当前是否可见
+    private boolean isVisible;
+    //设置Fragment可见或隐藏
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(getUserVisibleHint()) {
+            isVisible=true;
+            onVisible();
+        }else
+        {
+            isVisible=false;
+            onInVisible();
+        }
+    }
+    protected void onVisible()
+    {
+        //可见时实现懒加载
+        lazyLoad();
+    }
+
+    protected void onInVisible(){}
+    //子类实现懒加载
+    protected abstract void lazyLoad();
+}
