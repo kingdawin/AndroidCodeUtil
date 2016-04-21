474574f95d89338a0ef0e050fd7b227c08e7adda
kingdawin <397546372@qq.com>
2016-04-21 13:54:57 +0800

    add file

---
+++ AndroidCodeHelper/app/src/main/java/com/dawin/androidhelper/viewpager_fragment/SampleFragment.java
@@ -0,0 +1,12 @@
+package com.dawin.androidhelper.viewpager_fragment;
+
+/**
+ * Created by admin on 2016/4/15.da
+ */
+public class SampleFragment extends BaseFragment {
+
+    @Override
+    protected void lazyLoad() {
+
+    }
+}
